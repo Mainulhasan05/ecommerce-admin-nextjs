c806552945fb342d7cbd9f7095612cdb7aea76f9
Md. Mainul Hasan <55666627+Mainulhasan05@users.noreply.github.com>
2024-03-21 23:23:29 +0600

    go fot it

--- next.config.js
+++ next.config.js
@@ -3,11 +3,11 @@ const path = require('path')
 module.exports = {
   
   env:{
-    // API_URL: "https://api.mainulhasan05.xyz",
+    API_URL: "https://api.mainulhasan05.xyz",
     DEFAULT_LANGUAGE: 'bn',
     TINY_MCE_API:'2mhhga1agevzbqrjvzvqt8wbk4z0yowxqy6sgoaqssk9xezc',
     // API_URL: "https://api.rifatewu2.xyz",
-    API_URL:"http://localhost:5000"
+    // API_URL:"http://localhost:5000"
   },
   webpack: config => {
     config.resolve.alias = {
